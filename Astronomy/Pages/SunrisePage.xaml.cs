0088b120b432dca600a28a1e90b9085b43e0620e
MarynaSteniuk <stenyukmarina1@gmail.com>
2026-04-10 00:06:19 +0300

    Виконана 2 вправа 7 лабораторної

--- Astronomy/Pages/SunrisePage.xaml.cs
+++ Astronomy/Pages/SunrisePage.xaml.cs
@@ -2,39 +2,41 @@ namespace Astronomy.Pages;
 
 public partial class SunrisePage : ContentPage
 {
-    ILatLongService LatLongService { get; set; }
     public SunrisePage()
     {
         InitializeComponent();
-        LatLongService = new LatLongService();
     }
 
     protected override async void OnAppearing()
     {
         base.OnAppearing();
-        activityWaiting.IsRunning = true;
-        var sunriseSunsetData = await GetSunriseSunsetData();
-        InitializeUI(sunriseSunsetData.Item1, sunriseSunsetData.Item2, sunriseSunsetData.Item3);
-        activityWaiting.IsRunning = false;
+
+        var sunData = await GetSunriseSunsetData();
+
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            InitializeUI(sunData.Item1, sunData.Item2, sunData.Item3);
+        });
     }
 
-    async Task<(DateTime, DateTime, TimeSpan)> GetSunriseSunsetData()
+    private async Task<(DateTime, DateTime, TimeSpan)> GetSunriseSunsetData()
     {
-
-        var latLongData = await LatLongService.GetLatLong();
+        var latLongData = await new LatLongService().GetLatLong();
         var sunData = await new SunriseService().GetSunriseSunsetTimes(latLongData.Latitude, latLongData.Longitude);
 
         var riseTime = sunData.Sunrise.ToLocalTime();
         var setTime = sunData.Sunset.ToLocalTime();
         var span = setTime.TimeOfDay - riseTime.TimeOfDay;
+
         return (riseTime, setTime, span);
     }
 
     void InitializeUI(DateTime riseTime, DateTime setTime, TimeSpan span)
     {
+        
         lblDate.Text = DateTime.Today.ToString("D");
         lblSunrise.Text = riseTime.ToString("h:mm tt");
-        lblDaylight.Text = $"{span.Hours} hours, {span.Minutes} minutes";
         lblSunset.Text = setTime.ToString("h:mm tt");
+        lblDaylight.Text = $"{span.Hours} hours, {span.Minutes} minutes";
     }
 }
